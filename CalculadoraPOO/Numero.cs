cb9dd3b2062ec00521e27d60478f349e0f0878b9
marcos busto <marcosbustos694@gmail.com>
2019-03-06 11:58:47 -0300

    agregar logica

---
+++ CalculadoraPOO/Numero.cs
@@ -0,0 +1,77 @@
+﻿namespace CalculadoraPOO
+{
+    public class Numero
+    {
+        private int _Numero1;
+        private int _Numero2;
+        private int resto;
+        private int sum;
+
+        public int Numero1
+        {
+            get { return _Numero1; }
+            set { _Numero1 = value; }
+        }
+
+        public int Numero2
+        {
+            get { return _Numero2; }
+            set { _Numero2 = value; }
+        }
+
+        public int Sumar()
+        {
+            return Numero1 + Numero2;
+        }
+        public string Restar()
+        {
+            if (Numero1 >= Numero2)
+
+            {
+                for (int res = Numero2; res < Numero1; res++)
+                {
+                    resto++;
+                }
+                return resto.ToString();
+            }
+
+            else
+            {
+                for (int res = Numero1; res < Numero2; res++)
+                {
+                    resto++;
+                }
+
+                return "-"+ resto;   
+            }
+
+
+
+           
+        }
+        public double Multiplicar()
+        {
+            for (int k = 0; k < Numero2; ++k)
+            {  
+                sum += Numero1; 
+            }
+            return sum ;
+        }
+        public object Dividir()
+        {
+            if (Numero2 != 0)
+            {
+                for (int i = 0; i < Numero1; i = i + Numero2)
+                {
+                    resto++;
+                }
+                return resto;
+            }
+            else
+            {
+                return "No se puede dividir entre 0";
+            }
+
+        }
+    }
+}
--- CalculadoraPOO/Program.cs
+++ CalculadoraPOO/Program.cs
@@ -4,9 +4,49 @@ namespace CalculadoraPOO
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Console.WriteLine("Seleccione la operacion a realizar:\n1 - Sumar\n2 - Restar\n3 - Multiplicar\n4 - Dividir");
+
+            String opcion = Console.ReadLine();
+
+            if (opcion.Equals("1") || opcion.Equals("2") || opcion.Equals("3") || opcion.Equals("4"))
+            {
+                Numero Numero = new Numero();//Intanciar la clase
+
+                Console.Write("Ingrese valor 1: ");//Pedir numero 1
+                Numero.Numero1 = int.Parse(Console.ReadLine());//Leer numero ingresado
+
+                Console.Write("Ingrese valor 2: ");//Pedir Numero 2
+                Numero.Numero2 = int.Parse(Console.ReadLine());//Leer numero ingresado
+
+                if (opcion.Equals("1"))
+                {
+
+                    Console.WriteLine("El resultado es:" + Numero.Sumar());
+                }
+                if (opcion.Equals("2"))
+                {
+                    Console.WriteLine("El resultado es:" + Numero.Restar());
+                    
+
+                }
+                if (opcion.Equals("3"))
+                {
+                    Console.WriteLine("El resultado es:" + Numero.Multiplicar());
+                }
+                if (opcion.Equals("4"))
+                {
+                    Console.WriteLine("El resultado es:" + Numero.Dividir());
+                }
+            }
+
+
+            Console.WriteLine("Operacion finalizada!!");
+            Console.ReadKey();
         }
     }
 }
